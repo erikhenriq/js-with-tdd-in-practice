60029791969c2c3b82e3fcc6181c47a8c860580b
Erik Henrique <erik.hac@outlook.com>
2019-05-09 14:21:35 -0300

    Examples of describe, context and it

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,19 @@
-import { expect } from 'chai';
+/* import { expect  } from 'chai'; */
 
 describe('Main', () => {
+    describe('Method A', () => {
+        context('Case 1', () => {
+            it('should happen something', () => {
 
+            });
+        });
+
+        describe('Case 2', () => {
+
+        })
+    });
+
+    describe('Method B', () => {
+
+    });
 });
