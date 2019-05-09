5a765cfc87c0e82a2e67a0148543ebf10c2556c5
Erik Henrique <erik.hac@outlook.com>
2019-05-09 14:34:50 -0300

    Commands Skip, Only and Bail

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -5,12 +5,13 @@ describe('Main', () => {
         context('Case 1', () => {
             it('should happen something from Case 1', () => {
 
+                throw new Error('Just some error')
             });
         });
 
         context('Case 2', () => {
             it('should happen something from Case 2', () => {
-
+                throw new Error('Just some other error')
             });
 
             it('should happen other something from Case 2', () => {
