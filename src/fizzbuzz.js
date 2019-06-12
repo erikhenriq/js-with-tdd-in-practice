b92e040be88d255ab60149f7a1c2d3911fe1b73a
Erik Henrique <erik.hac@outlook.com>
2019-06-11 23:16:17 -0300

    Rodando check-coverage

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,19 +1,24 @@
 const FizzBuzz = (num) => {
-    if (num === 0) return 0;
+  if (num === 0) return 0;
 
-    if (num % 5 === 0 && num % 3 === 0) {
-        return 'FizzBuzz';
-    }
+  if (num % 5 === 0 && num % 3 === 0) {
+    return 'FizzBuzz';
+  }
 
-    if (num % 3 === 0) {
-        return 'Fizz';
-    }
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
 
-    if (num % 5 === 0) {
-        return 'Buzz';
-    }
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
 
-    return num;
+  return num;
 };
 
+function foo() {
+  return 'blah';
+}
+
+foo();
 export default FizzBuzz;
