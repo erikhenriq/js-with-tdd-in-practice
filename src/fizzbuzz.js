051d48ccfde6536bea10b8c9d95306bb93bb692a
Erik Henrique <erik.hac@outlook.com>
2019-06-11 23:22:02 -0300

    Remove função não usada

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -16,20 +16,4 @@ const FizzBuzz = (num) => {
   return num;
 };
 
-function foo() {
-  return 'blah';
-}
-
-function foo1() {
-  return 'blah';
-}
-
-function foo2() {
-  return 'blah';
-}
-
-function foo3() {
-  return 'blah';
-}
-
 export default FizzBuzz;
